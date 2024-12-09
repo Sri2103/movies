a82128363b6aabd5761a17ac0add3e829b4a7923
Sri2103 <damasriharsha@gmail.com>
2024-12-09 12:12:41 -0500

    running command test

--- cmd/rateingester/main.go
+++ cmd/rateingester/main.go
@@ -10,24 +10,33 @@ import (
 	"movieexample.com/rating/pkg/model"
 )
 
+const kafkaTImeOut = 10 * time.Second
+
 func main() {
 	fmt.Println("Creating a kafka producer...")
+
 	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
 	if err != nil {
 		panic(err)
 	}
 	defer producer.Close()
+
 	const fileName = "ratingsdara.json"
+
 	fmt.Println("Reading data from file: ", fileName)
+
 	ratings, err := readRatingsFromFile(fileName)
 	if err != nil {
 		panic(err)
 	}
+
 	err = produceRatingEvents("ratings", producer, ratings)
 	if err != nil {
 		panic(err)
 	}
-	timeout := 10 * time.Second
+
+	timeout := kafkaTImeOut
+
 	fmt.Println("Waiting for all messages to be delivered...")
 	producer.Flush(int(timeout.Milliseconds()))
 }
@@ -37,11 +46,19 @@ func readRatingsFromFile(fileName string) ([]model.RatingEvent, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
 	var ratings []model.RatingEvent
 	if err := json.NewDecoder(f).Decode(&ratings); err != nil {
 		return nil, err
 	}
+
 	return ratings, nil
 }
 
@@ -59,5 +76,6 @@ func produceRatingEvents(topic string, producer *kafka.Producer, events []model.
 			return err
 		}
 	}
+
 	return nil
 }
