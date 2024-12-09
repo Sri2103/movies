7afdd22c89b83379b069e6aeb39e16a5f855ddfb
Sri2103 <damasriharsha@gmail.com>
2024-12-09 15:24:18 -0500

    controller test with coverage

--- metadata/internal/controller/metadata/controller_test.go
+++ metadata/internal/controller/metadata/controller_test.go
@@ -2,11 +2,14 @@ package metadata
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel"
 	"go.uber.org/mock/gomock"
 	gen "movieexample.com/gen/mock/metadata/repository"
+	"movieexample.com/metadata/internal/repository"
 	"movieexample.com/metadata/pkg/model"
 )
 
@@ -24,12 +27,59 @@ func TestControllerPut(t *testing.T) {
 		Description: "description",
 		Director:    "director",
 	}
-	repoMock.EXPECT().Put(ctx, id, m).Return(nil)
+
+	Trctx, span := otel.Tracer("").Start(ctx, "TestControllerPut")
+	defer span.End()
+
+	repoMock.EXPECT().Put(Trctx, id, m).Return(nil)
 	err := c.Put(ctx, m)
 	assert.NoError(t, err)
 
-	repoMock.EXPECT().Get(ctx, id).Return(m, nil)
+	repoMock.EXPECT().Get(Trctx, id).Return(m, nil)
 	res, err := c.Get(ctx, id)
 	assert.NoError(t, err)
 	assert.Equal(t, m, res)
+}
+
+func TestController(t *testing.T) {
+	tests := []struct {
+		name       string
+		expRepoRes *model.Metadata
+		expRepoErr error
+		wantRes    *model.Metadata
+		wantErr    error
+	}{
+		{
+			name:       "not found",
+			expRepoErr: repository.ErrNotFound,
+			wantErr:    ErrNotFound,
+		},
+		{
+			name:       "unexpected error",
+			expRepoErr: errors.New("unexpected error"),
+			wantErr:    errors.New("unexpected error"),
+		},
+		{
+			name:       "success",
+			expRepoRes: &model.Metadata{},
+			wantRes:    &model.Metadata{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			repoMock := gen.NewMockmetadataRepository(ctrl)
+			c := New(repoMock)
+			ctx := context.Background()
+			id := "id"
+			Trctx, span := otel.Tracer("").Start(ctx, "TestControllerGet")
+			repoMock.EXPECT().Get(Trctx, id).Return(tt.expRepoRes, tt.expRepoErr)
+
+			defer span.End()
+			res, err := c.Get(ctx, id)
+			assert.Equal(t, tt.wantRes, res, tt.name)
+			assert.Equal(t, tt.wantErr, err, tt.name)
+		})
+	}
 }
