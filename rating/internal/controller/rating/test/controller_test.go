115d58334dd89c127cbc1207f8ea922b1ff13ca6
Sri2103 <damasriharsha@gmail.com>
2024-12-11 17:07:02 -0500

    testing rating put controller

--- rating/internal/controller/rating/test/controller_test.go
+++ rating/internal/controller/rating/test/controller_test.go
@@ -28,3 +28,29 @@ func TestControllerPut(t *testing.T) {
 	err := c.PutRating(ctx, model.RecordID(id), recordType, &rating)
 	assert.NoError(t, err)
 }
+
+func TestControllerAgg(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repoMock := gen.NewMockratingRepository(ctrl)
+	c := rating.NewController(repoMock, nil)
+	ctx := context.Background()
+	id := "id"
+	var ratings []model.Rating
+	ratings = []model.Rating{
+		{
+			UserID: model.UserID(id),
+			Value:  5,
+		},
+		{
+			UserID: model.UserID(id),
+			Value:  5,
+		},
+	}
+
+	recordType := model.RecordTypeMovie
+	repoMock.EXPECT().Get(ctx, model.RecordID(id), recordType).Return(ratings, nil)
+	res, err := c.GetAggregateRating(ctx, model.RecordID(id), recordType)
+	assert.NoError(t, err)
+	assert.Equal(t, float64(5), res)
+}
